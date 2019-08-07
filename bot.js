77626a0c2e45b955e7a89d9522c67a2c74216d19
Spence <sgormely01@gmail.com>
2019-08-07 02:26:01 -0400

    Version 1.0.0 upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+
+request.get(config.apiUrl, (error, response, body) => {
+    const server = JSON.parse(body);
+    const online = server.is_online
+    if (online == "1") {
+        const players = server.players;
+        const maxplayers = server.maxplayers;
+        return client.user.setActivity(`${players}/${maxplayers} on Teamspeak!`);
+    } else {
+        return client.user.setActivity(`Teamspeak is OFFLINE!`)
+    }
+
+});
+
+
+
+client.login(process.env.token || config.token);
+
+
